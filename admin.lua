fa972c471592dd24623e43f075bc0383760fd9bd
Teemsploit <53855235+Teemsploit@users.noreply.github.com>
2022-10-08 13:03:45 +0100

    Update admin.lua

--- admin.lua
+++ admin.lua
@@ -79,7 +79,26 @@ local commands = {
      --[[
     noclip = Humanoid:ChangeState(11), 
     ]]--
-    clear = rconsoleclear
+   
+	serverhop = function()
+        local x = {}
+        for _, v in ipairs(
+            game:GetService("HttpService"):JSONDecode(
+                game:HttpGetAsync(
+                    "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
+                )
+            ).data
+        ) do
+            if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
+                x[#x + 1] = v.id
+            end
+        end
+        if #x > 0 then
+            game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, x[math.random(1, #x)])
+        end
+    end,
+	
+	clear = rconsoleclear
 }
 
 function nexthandler()
