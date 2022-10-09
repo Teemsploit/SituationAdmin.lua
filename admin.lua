6bada9c5b181b4923af058b2b448dfccafaa5f25
AtpZombie42 <77166044+AtpZombie42@users.noreply.github.com>
2022-10-09 15:06:16 +0300

    importing usage

--- admin.lua
+++ admin.lua
@@ -97,17 +97,7 @@ commands = {
 		rconsoleprint('\n')
 	end, ]]
     serverhop = function(...)
-        local x = {}
-
-        for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
-            if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
-                x[#x + 1] = v.id
-            end
-        end
-
-        if #x > 0 then
-            game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, x[math.random(1, #x)])
-        end
+        import("serverhop.lua")
     end,
     antiafk = function(...)
         for i, v in pairs(getconnections(game:GetService("Players").LocalPlayer.Idled)) do
