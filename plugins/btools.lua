eb7e6bb18b629497084d15831184faf78bae6612
AtpZombie42 <77166044+AtpZombie42@users.noreply.github.com>
2022-10-09 16:20:01 +0300

    Create btools.lua

---
+++ plugins/btools.lua
@@ -0,0 +1,16 @@
+local game = game
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+local backpack = Player.Backpack
+hammer = Instance.new("HopperBin")
+hammer.Name = "Hammer"
+hammer.BinType = 4
+hammer.Parent = backpack
+cloneTool = Instance.new("HopperBin")
+cloneTool.Name = "Clone"
+cloneTool.BinType = 3
+cloneTool.Parent = backpack
+grabTool = Instance.new("HopperBin")
+grabTool.Name = "Grab"
+grabTool.BinType = 2
+grabTool.Parent = backpack
