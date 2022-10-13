1c6509374faabff094226bf6f995a7bbc76cc76e
HoppingShark1437 <115640582+HoppingShark1437@users.noreply.github.com>
2022-10-13 09:37:34 +0100

    Update ESP.lua

--- plugins/ESP.lua
+++ plugins/ESP.lua
@@ -1,5 +1,22 @@
-local GUI = loadstring(game:GetObjects("rbxassetid://10342057499")[1].Source)()
---[[ 
-protect gui errors and is removed in synapse v3
-syn.protect_gui(GUI)
-GUI.Parent = game:GetService("CoreGui") ]]
+--[[
+idk if this works made it in 5 mins
+]]
+
+local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kiriot22/ESP-Lib/main/ESP.lua"))()
+
+    if _G.Enabled == nil then _G.Enabled = true end
+    if _G.Boxes == nil then _G.Boxes = true end
+    if _G.TeamColors == nil then _G.TeamColors = true end
+    if _G.Tracers == nil then _G.Tracers = false end
+    if _G.Names == nil then _G.Names = true end
+	
+    while task.wait(.1) do
+        ESP:Toggle(_G.Enabled or false)
+        ESP.Boxes = _G.Boxes or false
+        ESP.TeamColors = _G.TeamColors or false
+        ESP.Tracers = _G.Tracers or false
+        ESP.Names = _G.Names or false
+    end
+
+    _G.Loaded = true
+end
