61d04182c9617e0af4d0d67481dc6e94255f2917
Misfortune <36631640+Drqonic@users.noreply.github.com>
2022-10-13 13:45:12 +0100

    Clean up

--- admin.lua
+++ admin.lua
@@ -3,6 +3,7 @@ if not game:IsLoaded() then
 end
 
 assert(rconsoleprint, "Your exploit is not supported!")
+
 local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
 local character = Player.Character
@@ -12,8 +13,7 @@ local User = Player.Name
 rconsolename("Welcome to Situation Admin | " .. User)
 
 local function getroot(char)
-	local rpart = char:FindFirstChild("HumanoidRootPart") or char:FindFirstChild("Torso") or char:FindFirstChild("UpperTorso")
-	return rpart
+	return char:FindFirstChild("HumanoidRootPart") or char:FindFirstChild("Torso") or char:FindFirstChild("UpperTorso")
 end
 
 local import = function(asset)
@@ -24,6 +24,7 @@ local import = function(asset)
 		local resp = game:HttpGetAsync(link)
 		local callfunc = loadstring(resp)
 		local Success, weget = pcall(callfunc)
+
 		if Success then
 			return weget
 		else
@@ -33,12 +34,13 @@ local import = function(asset)
 end
 
 commands = {
-    help = function(...)
+	help = function(...)
 		for key, value in pairs(commands) do
 			rconsoleprint("@@WHITE@@")
 			rconsoleprint(key .. '\n')
 		end
 	end,
+	
     lagserver = function(...)
 		for _, v in pairs(getconnections(game:GetService("ScriptContext").Error)) do
 			v:Disable()
@@ -54,6 +56,7 @@ commands = {
 			end
 		end
 	end,
+	
 	deletetouchinterests = function(...)
 		for _, v in ipairs(workspace:GetDescendants()) do
 			if v.IsA(v, "TouchTransmitter") then
@@ -61,67 +64,78 @@ commands = {
 			end
 		end
 	end,
+	
 	networkownership = function(...)
-		local function network()
-			local Render = game:GetService("RunService").RenderStepped
-			local Huge = math.huge * math.huge
-			local BigHuge = math.pow(math.huge, math.huge)
-			Render:Connect(function()
-				sethiddenproperty(Player, 'MaximumSimulationRadius', BigHuge)
-				sethiddenproperty(Player, 'SimulationRadius', Huge)
-			end)
-		end
-		network()
+		local Render = game:GetService("RunService").RenderStepped
+		local Huge = math.huge * math.huge
+		local BigHuge = math.pow(math.huge, math.huge)
+		
+		Render:Connect(function()
+			sethiddenproperty(Player, 'MaximumSimulationRadius', BigHuge)
+			sethiddenproperty(Player, 'SimulationRadius', Huge)
+		end)
 	end,
+	
     spoofmemory = function(...)
 		hookfunction((gcinfo or collectgarbage), function(...)
 			return math.random(200, 350)
 		end)
-		task.wait(0.01)
+		
 		local gamemt = getrawmetatable(game)
+		
 		setreadonly(gamemt, false)
+		
 		local nc = gamemt.__namecall
+		
 		gamemt.__namecall = newcclosure(function(...)
-			if (getnamecallmethod() == 'GetTotalMemoryUsageMb') then
+			if (getnamecallmethod() == "GetTotalMemoryUsageMb") then
 				return math.random(395, 405)
 			end
+
 			return nc(...)
 		end)
+		
 		hookfunction(game.Stats.GetTotalMemoryUsageMb, function()
 			return math.random(395, 405)
 		end)
-		task.wait()
+		
 		rconsoleprint("Memory Spoofed! \n")
 	end,
-        
+	
     teleport = function(...)
 		for i, plr in ipairs(Players:GetPlayers()) do
 			rconsoleprint(i .. ".) " .. plr.Name .. "\n")
 		end
+		
 		rconsoleprint("Choice: ")
+		
 		local playerNum = tonumber(rconsoleinput())
-		local players = Players:GetPlayers()
-		if playerNum ~= nil and playerNum > 0 and playerNum <= #players then
-			local plrchar = players[playerNum].Character
+		
+		if playerNum ~= nil and playerNum > 0 and playerNum <= #Players then
+			local plrchar = Players[playerNum].Character
+			
 			getroot(character).CFrame = getroot(plrchar).CFrame
 		else
 			rconsolewarn("Player chosen is invalid!\n")
 		end
 	end,
+	
     bitchcount = function(...)
-		local rnumb = math.random(1, 100)
-		rconsolewarn("You have " .. rnumb .. " bitches")
+		rconsolewarn("You have " .. math.random(1, 100) .. " bitches")
 	end,
+	
     joinlogs = function(...)
-		local args = {
-			...
-		}
+		local args = {...}
+		
+		-- Check if true or false
 		joinlogsenabled = args[1] or not joinlogsenabled
+		
 		if joinlogsenabled then
 			joinlogcon = Players.PlayerAdded:Connect(function(plr)
 				rconsoleprint("@@WHITE@@")
 				rconsoleprint("\n" .. plr.Name .. " has joined, account is " .. plr.AccountAge .. " days old!\n")
 			end)
+			
 			leavelogcon = Players.PlayerRemoving:Connect(function(plr)
 				rconsoleprint("@@WHITE@@")
 				rconsoleprint("\n" .. plr.Name .. " has left!\n")
@@ -131,95 +145,103 @@ commands = {
 			leavelogcon = leavelogcon:Disconnect()
 		end
 	end,
+	
     btools = function(...)
 		import("btools.lua")
 	end,
+	
     funfact = function(...)
-		rconsoleprint(game:GetService("HttpService"):JSONDecode(game:HttpGet("https://uselessfacts.jsph.pl/random.json?language=en")).text)
-		rconsoleprint("\n")
+		rconsoleprint(game:GetService("HttpService"):JSONDecode(game:HttpGet("https://uselessfacts.jsph.pl/random.json?language=en")).text .. "\n")
 	end,
+	
     walkspeed = function(...)
-		local args = {
-			...
-		}
+		local args = {...}
 		local ws = args[1]
+		
 		Humanoid.WalkSpeed = ws
 	end,
-        jumppower  = function(...)
-		local args = {
-			...
-		}
+	
+	jumppower  = function(...)
+		local args = {...}
 		local jp = args[1]
+		
 		Humanoid.JumpPower = jp
 	end,
-           hipheight  = function(...)
-		local args = {
-			...
-		}
+	
+	hipheight  = function(...)
+		local args = {...}
 		local hh = args[1]
+		
 		Humanoid.HipHeight = hh
 	end,
+	
     avatar = function(...)
-		local args = {
-			...
-		}
+		local args = {...}
 		local id = args[1]
 		local newmodel = Players:GetCharacterAppearanceAsync(id)
+
 		character = newmodel
+		
 		rconsolewarn("This is client-side only!\n")
 	end,
+	
     serverhop = function(...)
 		import("serverhop.lua")
 	end,
+	
     esp = function(...)
 		import("ESP.lua")
 	end,
+	
     antiafk = function(...)
 		for i, v in pairs(getconnections(game:GetService("Players").LocalPlayer.Idled)) do
 			v:Disable()
 		end
 	end,
+	
     chat = function(...)
-		local args = {
-			...
-		}
-		local Message = args[1]
+		local args = {...}
+		local message = args[1]
 		local storage = game:GetService("ReplicatedStorage")
+		
 		if storage:FindFirstChild("DefaultChatSystemChatEvents") then
-			storage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(Message, "All")
+			storage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
 		else
-			rconsolewarn("It seems this game has a custom chat this command will not work.")
+			rconsolewarn("It seems this game has a custom chat this command will not work.\n")
 		end
 	end,
     
     cocksize = function(...)
-		local rnumb = math.random(1, 600)
-		rconsolewarn("Your cock is " .. rnumb / 100 .. " inches.")
+		rconsolewarn("Your cock is " .. math.random(1, 600) / 100 .. " inches.")
 	end,
     --[[
 	Wont Work
     noclip = Humanoid:ChangeState(11), ]]
-    clear = rconsoleclear}
+    clear = rconsoleclear
+}
 
-function nexthandler()
+while task.wait() do
 	rconsoleprint("@@WHITE@@")
 	rconsoleprint("Input: ")
+
 	local args = rconsoleinput()
 	local tokens = {}
+
 	for v in string.gmatch(args, "([^ ]+)") do
 		table.insert(tokens, v)
 	end
+
 	local command = string.lower(tokens[1])
 	local getCommand = commands[command]
+
 	table.remove(tokens, 1)
+
 	if getCommand then
 		commands[command](table.unpack(tokens))
+	
 		rconsoleprint("@@GREEN@@")
 		rconsoleprint("Executed " .. command .. " successfully!\n")
 	else
 		rconsoleerr("Failed to execute " .. command .. "!\n")
 	end
-	nexthandler()
 end
-
-nexthandler()
