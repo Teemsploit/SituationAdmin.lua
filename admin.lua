875c070defa8bf91512f8b0a82ffcda133944a17
Misfortune <36631640+Drqonic@users.noreply.github.com>
2022-10-13 17:32:32 +0100

    Update admin.lua

--- admin.lua
+++ admin.lua
@@ -48,31 +48,12 @@ commands = {
 			rconsoleprint(key .. '\n')
 		end
 	end,
-	
-	btools = function(...)
-    hammer = Instance.new("HopperBin")
-    hammer.Name = "Hammer"
-    hammer.BinType = 4
-    hammer.Parent = backpack
-    cloneTool = Instance.new("HopperBin")
-    cloneTool.Name = "Clone"
-    cloneTool.BinType = 3
-    cloneTool.Parent = backpack
-    grabTool = Instance.new("HopperBin")
-    grabTool.Name = "Grab"
-    grabTool.BinType = 2
-    grabTool.Parent = backpack
-end,
-	
-	noproximitycooldown = function(...)
-		
-		game:GetService("ProximityPromptService").PromptButtonHoldBegan:Connect(fireproximityprompt)
-		end,
 
-for _, v in pairs(workspace:GetDescendants()) do
-    check(v)
-end
-    lagserver = function(...)
+	noproximitycooldown = function(...)
+		game:GetService("ProximityPromptService").PromptButtonHoldBegan:Connect(fireproximityprompt)
+	end,
+
+	lagserver = function(...)
 		for _, v in pairs(getconnections(game:GetService("ScriptContext").Error)) do
 			v:Disable()
 		end
@@ -107,7 +88,7 @@ end
 		end)
 	end,
 	
-    spoofmemory = function(...)
+	spoofmemory = function(...)
 		hookfunction((gcinfo or collectgarbage), function(...)
 			return math.random(200, 350)
 		end)
@@ -133,7 +114,7 @@ end
 		rconsoleprint("Memory Spoofed! \n")
 	end,
 	
-    teleport = function(...)
+	teleport = function(...)
 		for i, plr in ipairs(Players:GetPlayers()) do
 			rconsoleprint(i .. ".) " .. plr.Name .. "\n")
 		end
@@ -151,11 +132,11 @@ end
 		end
 	end,
 	
-    bitchcount = function(...)
+	bitchcount = function(...)
 		rconsolewarn("You have " .. math.random(1, 100) .. " bitches")
 	end,
 	
-    joinlogs = function(...)
+	joinlogs = function(...)
 		local args = {...}
 		
 		-- Check if true or false
@@ -177,11 +158,31 @@ end
 		end
 	end,
 	
-    funfact = function(...)
-		rconsoleprint(httpservice:JSONDecode(game:HttpGet("https://uselessfacts.jsph.pl/random.json?language=en")).text .. "\n")
+	btools = function(...)
+		local Player = Players.LocalPlayer
+		local backpack = Player.Backpack
+
+		hammer = Instance.new("HopperBin")
+		hammer.Name = "Hammer"
+		hammer.BinType = 4
+		hammer.Parent = backpack
+
+		cloneTool = Instance.new("HopperBin")
+		cloneTool.Name = "Clone"
+		cloneTool.BinType = 3
+		cloneTool.Parent = backpack
+
+		grabTool = Instance.new("HopperBin")
+		grabTool.Name = "Grab"
+		grabTool.BinType = 2
+		grabTool.Parent = backpack
 	end,
 	
-    walkspeed = function(...)
+	funfact = function(...)
+		rconsoleprint(game:GetService("HttpService"):JSONDecode(game:HttpGet("https://uselessfacts.jsph.pl/random.json?language=en")).text .. "\n")
+	end,
+	
+	walkspeed = function(...)
 		local args = {...}
 		local ws = args[1]
 		
@@ -202,7 +203,7 @@ end
 		Humanoid.HipHeight = hh
 	end,
 	
-    avatar = function(...)
+	avatar = function(...)
 		local args = {...}
 		local id = args[1]
 		local newmodel = Players:GetCharacterAppearanceAsync(id)
@@ -212,38 +213,42 @@ end
 		rconsolewarn("This is client-side only!\n")
 	end,
 	
-    serverhop = function(...)
+	serverhop = function(...)
 		local x = {}
-local httpservice = httpservice
-local pid = game.PlaceId
-local jid = game.JobId
-local teleportservice = game:GetService("TeleportService")
+		local http = game:GetService("HttpService")
+		local pid = game.PlaceId
+		local jid = game.JobId
+		local tps = game:GetService("TeleportService")
 
-for _, v in ipairs(httpservice:JSONDecode(game:HttpGetAsync("htteleportservice://games.roblox.com/v1/games/" .. pid .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
-    
-    --[[ method is slow ]]
-    
-    if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= jid then
-        x[#x + 1] = v.id
-    end
-end
+		for _, v in ipairs(http:JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. pid .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
+		    
+		    --[[ method is slow ]]
+		    
+		    if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= jid then
+		        x[#x + 1] = v.id
+		    end
+		end
 
-if #x > 0 then
-    teleportservice:TeleportToPlaceInstance(pid, x[math.random(1, #x)])
-end
+		if #x > 0 then
+		    tps:TeleportToPlaceInstance(pid, x[math.random(1, #x)])
+		end
 	end,
 	
-    esp = function(...)
-		import("ESP.lua")
+	esp = function(...)
+		local GUI = loadstring(game:GetObjects("rbxassetid://10342057499")[1].Source)()
+
+		syn.protect_gui(GUI)
+		
+		GUI.Parent = game:GetService("CoreGui")
 	end,
 	
-    antiafk = function(...)
+	antiafk = function(...)
 		for i, v in pairs(getconnections(game:GetService("Players").LocalPlayer.Idled)) do
 			v:Disable()
 		end
 	end,
 	
-    chat = function(...)
+	chat = function(...)
 		local args = {...}
 		local message = args[1]
 		local storage = game:GetService("ReplicatedStorage")
@@ -254,14 +259,15 @@ end
 			rconsolewarn("It seems this game has a custom chat this command will not work.\n")
 		end
 	end,
-    
-    cocksize = function(...)
+	
+	cocksize = function(...)
 		rconsolewarn("Your cock is " .. math.random(1, 600) / 100 .. " inches.")
 	end,
-    --[[
+	--[[
 	Wont Work
-    noclip = Humanoid:ChangeState(11), ]]
-    clear = rconsoleclear
+	noclip = Humanoid:ChangeState(11), 
+	]]
+	clear = rconsoleclear
 }
 
 while task.wait() do
