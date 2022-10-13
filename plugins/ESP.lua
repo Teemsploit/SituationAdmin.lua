ed26df8d78402aed12af2acf007378faf22ae1f9
HoppingShark1437 <115640582+HoppingShark1437@users.noreply.github.com>
2022-10-13 09:14:39 +0100

    Update ESP.lua

--- plugins/ESP.lua
+++ plugins/ESP.lua
@@ -1,3 +1,5 @@
 local GUI = loadstring(game:GetObjects("rbxassetid://10342057499")[1].Source)()
+--[[ 
+protect gui errors and is removed in synapse v3
 syn.protect_gui(GUI)
-GUI.Parent = game:GetService("CoreGui")
+GUI.Parent = game:GetService("CoreGui") ]]
