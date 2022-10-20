97271a20ace6288f223628ce0f9d0899b44eaaee
Misfortune <36631640+Drqonic@users.noreply.github.com>
2022-10-20 20:02:10 +0100

    Update admin.lua

--- admin.lua
+++ admin.lua
@@ -3,7 +3,7 @@ if not game:IsLoaded() then
 	game.Loaded:Wait()
 end
 
-messagebox('Please note "Situation admin" is in beta and you may experience bugs.\nType "help" in the console for a list of commands\nif you need further support please join the discord server https://discord.gg/5VAKhPQh8Z\nCredits:\nTeem\nDrqonic\nionized particle/AtpZombie42\nHoppingShark1437','Situation Admin',0)
+messagebox('Please note "Situation admin" is in beta and you may experience bugs.\nType "help" in the console for a list of commands\nif you need further support please join the discord server https://discord.gg/5VAKhPQh8Z\nCredits:\nTeem\nDrqonic\nionized particle/AtpZombie42\nHoppingShark1437', 'Situation Admin', 0)
 
 assert(rconsoleprint, "Your exploit is not supported!")
 assert(fireproximityprompt, "Your exploit is not supported!")
