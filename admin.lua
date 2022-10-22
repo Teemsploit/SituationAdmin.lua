070c36b79b944dad84e5d7c600b22e51bee0c8a1
Teemsploit <53855235+Teemsploit@users.noreply.github.com>
2022-10-22 15:13:00 +0100

    Update admin.lua

--- admin.lua
+++ admin.lua
@@ -4,7 +4,7 @@ end
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Alteral323/v/main/libs/compat.lua"))()(true)
 
-messagebox('Please note "Situation admin" is in beta and you may experience bugs.\nType "help" in the console for a list of commands\nif you need further support please join the discord server https://discord.gg/5VAKhPQh8Z\nCredits:\nTeem\nDrqonic\nIonized particle/AtpZombie42\nHoppingShark1437\nToon\nNeatdev', 'Situation Admin', 0)
+messagebox('Please note "Situation admin" is in beta and you may experience bugs.\nType "help" in the console for a list of commands\nif you need further support please join the discord server https://discord.gg/5VAKhPQh8Z\nCredits:\nTeem\nDrqonic\nIonized particle/AtpZombie42\nHoppingShark1437\nToon', 'Situation Admin', 0)
 -- by clicking 'OK' you have signed your soul over to Teem
 assert(rconsoleprint, "Your exploit is not supported!")
 assert(fireproximityprompt, "Your exploit is not supported!")
