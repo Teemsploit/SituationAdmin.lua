37ed5070c2b241fd9d0696c7af3270bafb21c0d2
Teemsploit <53855235+Teemsploit@users.noreply.github.com>
2022-11-02 20:18:35 +0000

    Update admin.lua

--- admin.lua
+++ admin.lua
@@ -11,6 +11,7 @@ getgenv().SituationAdmin = true ]]
 
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Alteral323/v/main/libs/compat.lua"))()(true)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Teemsploit/SituationAdmin.lua/main/IrisBetterConsole.lua"))()
 
 messagebox('Please note "Situation admin" is in beta and you may experience bugs or broken commands.\nType "help" in the console for a list of commands\nif you need further support please join the discord server https://discord.gg/5VAKhPQh8Z\nCredits:\nTeem\nDrqonic\nIonized particle/AtpZombie42\nHoppingShark1437\nToon', 'Situation Admin', 0)
 -- by clicking 'OK' you have signed your soul over to Teem
