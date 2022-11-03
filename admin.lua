f4c6fefc04851f76df74d772b93dab0e8cf55b08
Teemsploit <53855235+Teemsploit@users.noreply.github.com>
2022-11-03 21:26:15 +0000

    Update admin.lua

--- admin.lua
+++ admin.lua
@@ -11,7 +11,7 @@ getgenv().SituationAdmin = true ]]
 
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Alteral323/v/main/libs/compat.lua"))()(true)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Teemsploit/SituationAdmin.lua/main/IrisBetterConsole.lua"))()
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/Teemsploit/SituationAdmin.lua/main/IrisBetterConsole.lua"))()
 
 messagebox('Please note "Situation admin" is in beta and you may experience bugs or broken commands.\nType "help" in the console for a list of commands\nif you need further support please join the discord server https://discord.gg/5VAKhPQh8Z\nCredits:\nTeem\nDrqonic\nIonized particle/AtpZombie42\nHoppingShark1437\nToon', 'Situation Admin', 0)
 
